0ddd21754f0ceb384cca13f90fa479da3c58d1cc
Ivan <jwplaster.com@gmail.com>
2021-08-21 19:30:53 +0300

    Добавьте файлы проекта.

---
+++ AttachmentsService/AttachmentsService.API/Configurations/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,28 @@
+﻿using AttachmentsService.Data;
+using AttachmentsService.Data.Dto;
+using AttachmentsService.Data.ViewModel.Input;
+using AttachmentsService.Data.Entity;
+using AutoMapper;
+using AutoMapper.EquivalencyExpression;
+using System;
+using System.Linq;
+
+namespace AttachmentsService.API.Configurations.AutoMapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<AttachmentViewModel, AttachmentDto>()
+                .ForMember(x => x.FileName, s => s.MapFrom(y => y.File.FileName))
+                .ForMember(x => x.Size, s => s.MapFrom(y => y.File.Length))
+                .ForMember(x => x.Stream, s => s.MapFrom(y => y.File.OpenReadStream()))
+                .ForMember(x => x.File, s => s.Ignore())
+                .ForMember(x => x.DeathDate, s => s.MapFrom(y => y.LifeTime));
+
+            CreateMap<Attachment, AttachmentDto>();
+
+            CreateMap<AttachmentDto, Attachment>();
+        }
+    }
+}
---
+++ AttachmentsService/AttachmentsService.Data/ViewModels/Input/AttachmentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace AttachmentsService.Data.ViewModel.Input
+{
+    public class AttachmentViewModel
+    {
+        public DateTime LifeTime { get; set; }
+
+        public IFormFile File { get; set; }
+    }
+}
